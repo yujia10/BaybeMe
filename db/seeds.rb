d09b945956c3354891b09ef145e4848ff2a3ab50
Yujia Gu <yujiagu1010@gmail.com>
2022-06-07 14:42:50 +0930

    test2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Childcare.destroy_all
+
+childcare1 = Childcare.new(name: "Mawson Lakes Preschool", email: "childcare@gmail.com", url: "https://mawsonlakespre.sa.edu.au/", address: "259 middleborough rd, box hill", description: "the best cc you can find")
+
+childcare1.save!
+
+childcare2 = Childcare.new(name: "Green Leaves Early Learning Mawson Lakes", email: "newcare@gmail.com", url: "https://mawsonlakes.com/", address: "200 middleborough rd, box hill", description: "the best cc you can find")
+
+childcare2.save!
