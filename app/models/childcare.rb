3424f6ddbbd70477a92cdc9fd194e5be5eae813b
Julien Voraz <julien1voraz@gmail.com>
2022-06-07 14:43:30 +1000

    243pm seeds

--- app/models/childcare.rb
+++ app/models/childcare.rb
@@ -3,4 +3,5 @@ class Childcare < ApplicationRecord
   has_many :users, through: :applications
   validates :name, :url, presence: true
   validates :address, :email, presence: true, uniqueness: true
+  has_one_attached :photo
 end
