ac05c88a49b83f0d136f7c5ada53087d7feb2451
Yujia Gu <yujiagu1010@gmail.com>
2022-06-14 15:44:37 +0930

    fix application card style

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,11 +27,9 @@ Application.destroy_all
 
 phillip = User.create!(first_name: "Phillip", last_name: "Drummond", phone_number: "0421458844", address: "10 Janice Rd, Glen Waverley VIC 3150", email: "phillip@drummond.com", password: "secret")
 alan = User.create!(first_name: "Alan", last_name: "Harper", phone_number: "0466804477", address: "256 Ferntree Gully Rd, Notting Hill VIC 3168", email: "alan@harper.com", password: "secret")
-yu = User.create!(first_name: "Yu", last_name: "Gu", phone_number: "0419847566", address: "02 Franklin Rd, Magill SA 3168", email: "yujiagu1010@gmail.com", password: "secret")
 
 # **************CHILDREN SEEDS*********************
 
 arnold = Child.create!(first_name: "Arnold", last_name: "Jackson", gender: 1, dob: Date.new(2020,10,12), user: phillip)
 willis = Child.create!(first_name: "Willis", last_name: "Jackson", gender: 1, dob: Date.new(2019,12,29), user: phillip)
 jake = Child.create!(first_name: "Jake", last_name: "Harper", gender: 1, dob: Date.new(2020,03,01), user: alan)
-jia = Child.create!(first_name: "Jia", last_name: "Gu", gender: 1, dob: Date.new(2020,03,01), user: yu)
